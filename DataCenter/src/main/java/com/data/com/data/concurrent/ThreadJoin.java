c30bad6e9c641e3d57cf7ff2e99a070924efba82
ShakeCode <903936584@qq.com>
2019-02-21 00:38:10 +0800

    exchanger test

--- DataCenter/src/main/java/com/data/com/data/concurrent/ThreadJoin.java
+++ DataCenter/src/main/java/com/data/com/data/concurrent/ThreadJoin.java
@@ -13,7 +13,42 @@ public class ThreadJoin {
 //        CyclicBarrierTest();
 //        CylicBarrierTest2();    //优先执行情况
 //        SemaphoreTest();
+//        ExchangerTest();
 
+    }
+
+    private static void ExchangerTest() {
+    /*  Exchanger（交换者）是一个用于线程间协作的工具类。Exchanger用于进行线程间的数据交
+      换。它提供一个同步点，在这个同步点，两个线程可以交换彼此的数据。这两个线程通过
+      exchange方法交换数据，如果第一个线程先执行exchange()方法，它会一直等待第二个线程也
+      执行exchange方法，当两个线程都到达同步点时，这两个线程就可以交换数据，将本线程生产
+      出来的数据传递给对方。*/
+        final Exchanger<String> exgr = new Exchanger<String>();    //线程协助工具类
+        ExecutorService threadPool = Executors.newFixedThreadPool(2);
+        threadPool.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    String A = "银行流水A"; // A录入银行流水数据
+                    exgr.exchange(A);
+                } catch (InterruptedException e) {
+                }
+            }
+        });
+        threadPool.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    String B = "银行流水B"; // B录入银行流水数据
+                    String A = exgr.exchange(B);
+                    System.out.println("交换数据:"+A);
+                    System.out.println("A和B数据是否一致：" + A.equals(B) + "，A录入的是："
+                            + A + "，B录入是：" + B);
+                } catch (InterruptedException e) {
+                }
+            }
+        });
+        threadPool.shutdown();
     }
 
     private static void SemaphoreTest() {

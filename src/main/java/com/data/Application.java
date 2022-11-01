e822cfb5ada09e1a492a4c0099e72e88a76413aa
ShakeCode <903936584@qq.com>
2022-11-01 22:22:15 +0800

    fix(*): 设置服务统一0时区

--- src/main/java/com/data/Application.java
+++ src/main/java/com/data/Application.java
@@ -1,12 +1,17 @@
 package com.data;
 
 import com.data.service.custer.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.Date;
+import java.util.TimeZone;
 
 /*解决占用问题
 
@@ -25,10 +30,13 @@ taskkill /f /t  /im java.exe*/
 @EnableAsync
 @SpringBootApplication(scanBasePackages = {"com.data"})
 public class Application {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
 
     private static ConfigurableApplicationContext configurableApplicationContext;
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        LOGGER.error("现在的0时区时间:{}", new Date());
         configurableApplicationContext = SpringApplication.run(Application.class, args);
 //        xml 形式 可使用的上下文
 //        FileSystemXmlApplicationContext
